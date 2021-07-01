76a89ffc3928cca5b7962768b71bf9065a0ba5e2
tatsuya-hashimoto <tatsuya.hashimoto@dena.jp>
2021-07-02 03:46:10 +0900

    板と手にmethod追加

--- domain/game/action/action.go
+++ domain/game/action/action.go
@@ -9,14 +9,34 @@ import (
 type Item struct {
 	x      int
 	y      int
-	status constants.NodeStatus
+	simbol constants.NodeStatus
 }
 
-func NewItem(x, y int, staus constants.NodeStatus) (*Item, error) {
+func NewItem(x, y int, simbol constants.NodeStatus) (*Item, error) {
 	if x < 0 || 2 < x {
 		return nil, errors.New("x: out of range")
 	}
 	if y < 0 || 2 < y {
 		return nil, errors.New("y: out of range")
 	}
+	if simbol == constants.NONE {
+		return nil, errors.New("invalid simbol")
+	}
+	return &Item{
+		x:      x,
+		y:      y,
+		simbol: simbol,
+	}, nil
+}
+
+func (i *Item) X() int {
+	return i.x
+}
+
+func (i *Item) Y() int {
+	return i.y
+}
+
+func (i *Item) Simbol() constants.NodeStatus {
+	return i.simbol
 }
--- domain/game/board/board.go
+++ domain/game/board/board.go
@@ -1,6 +1,11 @@
 package board
 
-import "github.com/TTRSQ/CircleCrossGame/domain/game/action"
+import (
+	"errors"
+
+	"github.com/TTRSQ/CircleCrossGame/domain/game/action"
+	"github.com/TTRSQ/CircleCrossGame/domain/game/constants"
+)
 
 /*
 task:
@@ -9,10 +14,71 @@ task:
 - 現状調査
 */
 
-type Board struct {
-	status [][]int
+const (
+	WIDTH  = 3
+	HEIGHT = 3
+)
+
+type Board []constants.NodeStatus
+
+func NewBoard() Board {
+	return Board{
+		constants.NONE, constants.NONE, constants.NONE,
+		constants.NONE, constants.NONE, constants.NONE,
+		constants.NONE, constants.NONE, constants.NONE,
+	}
 }
 
-func (b *Board) Act(act action.Item) error {
+func (b Board) Act(act action.Item) error {
+	if b[act.Y()*WIDTH+act.X()] != constants.NONE {
+		return errors.New("already placed")
+	}
+	b[act.Y()*WIDTH+act.X()] = act.Simbol()
 	return nil
+}
+
+func lineBinaryList() []int {
+	return []int{
+		448, // 111,000,000
+		56,  // 000,111,000
+		7,   // 000,000,111
+		292, // 100,100,100
+		146, // 010,010,010
+		73,  // 001,001,001
+		273, // 100,010,001
+		84,  // 001,010,100
+	}
+}
+
+func (b Board) Binary(simbol constants.NodeStatus) (int, error) {
+	if simbol == constants.NONE {
+		return 0, errors.New("invald simbol")
+	}
+	stamp := []int{
+		512, 256, 128,
+		64, 32, 16,
+		8, 4, 2,
+	}
+	sum := 0
+	for i := range b {
+		if b[i] == simbol {
+			sum += stamp[i]
+		}
+	}
+	return sum, nil
+}
+
+func (b Board) CountLine(simbol constants.NodeStatus) (int, error) {
+	if simbol == constants.NONE {
+		return 0, errors.New("invald simbol")
+	}
+
+	binary, _ := b.Binary(simbol)
+	count := 0
+	for _, line := range lineBinaryList() {
+		if binary&line == line {
+			count += 1
+		}
+	}
+	return count, nil
 }
